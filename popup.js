345b344ca65744de7c369df021bda06d33f28761
vamp-r <pabloperez96@gmail.com>
2021-11-05 12:54:04 +0100

    Hide console.log

--- popup.js
+++ popup.js
@@ -7,32 +7,32 @@ getValue(function(value) {
 
 result1=value;
 result1=result1.stor;
-console.log("Result1= "+result1);
+//console.log("Result1= "+result1);
 if (result1=="on"){
-  console.log("Abierto")
+  //console.log("Abierto")
   document.getElementById("accept").checked = true; //cambia la posición del botón
 }else{
-  console.log("Cerrado")
+  //console.log("Cerrado")
   document.getElementById("accept").checked = false;
 };
 });
 
 
 
-console.log("Result1= "+result1);
+//console.log("Result1= "+result1);
 var checkbox = document.querySelector("input[name=accept]");
 
 checkbox.addEventListener('change', function() {
   if (this.checked) {
     chrome.storage.sync.set({"stor": "on"}, () => {
     });
-    console.log("Checkbox is checked..");
+    //console.log("Checkbox is checked..");
 
     function getValue(callback) {
       chrome.storage.sync.get("stor", callback);
    }
    getValue(function(value) {
-    console.log(value);
+    //console.log(value);
     chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
       chrome.tabs.reload(arrayOfTabs[0].id);
   });
@@ -40,13 +40,13 @@ checkbox.addEventListener('change', function() {
   } else {
     chrome.storage.sync.set({"stor": "off"}, () => {
     });
-    console.log("Checkbox is not checked..");
+    //console.log("Checkbox is not checked..");
 
     function getValue(callback) {
       chrome.storage.sync.get("stor", callback);
    }
    getValue(function(value) {
-    console.log(value);
+    //console.log(value);
     chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
       chrome.tabs.reload(arrayOfTabs[0].id);
   });
